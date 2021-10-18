b2eba56aaac5a65b3355965237502e1664f869c8
dialectrical <michael.camele@gmail.com>
2021-10-18 16:11:34 -0700

    feat: function takes an integer as input now and can fizzbuzz an arbitrary amount

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,5 +1,5 @@
-const fizzBuzz = () => {
-  for (let i = 1; i <= 100; i++) {
+const fizzBuzz = (num) => {
+  for (let i = 1; i <= num; i++) {
     if (i % 15 === 0) {
       console.log("FizzBuzz");
     } else if (i % 3 === 0) {
@@ -14,4 +14,4 @@ const fizzBuzz = () => {
   return;
 };
 
-fizzBuzz();
+fizzBuzz(100);
